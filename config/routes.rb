45a83498946777be06bb2b44df90cf62ba776273
Ivan Yip <ivan_yip@msn.com>
2017-04-06 07:45:27 +0800

    5/4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   root to: 'videos#index'
-  resources :videos, only: [:index, :show]
+  resources :videos, only: [:show] do
+    collection do
+      post :search, to: "videos#search"
+    end
+  end
   resources :categories, only: [:show]
 end
